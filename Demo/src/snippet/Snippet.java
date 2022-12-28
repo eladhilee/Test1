a971e6e907e54bb1caabea6bfeaf8413ace22fd8
eladhilee <eladhilee@gmail.com>
2022-12-28 13:12:25 +0530

    demo

---
+++ Demo/src/snippet/Snippet.java
@@ -0,0 +1,8 @@
+package snippet;
+
+public class Snippet {
+	public static void main(String[] args) {
+		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
+	}
+}
+
